1e9d63891cb30298948258d436f21f5046ed521a
fasteddiegarcia <fasteddiegarcia@gmail.com>
2016-03-03 21:04:26 -0600

    Models collections and views update

--- public/js/app.js
+++ public/js/app.js
@@ -11,18 +11,53 @@ var PostModel = Backbone.Model.extend({
     idAttribute: 'id',
 });
 
+var SubbredditModel = Backbone.Model.extend({
+    urlRoot: '/api/subbreddits/',
+    idAttribute: 'id'
+});
+
+var CommentModel = Backbone.Model.extend({
+    urlRoot: '/api/comments/',
+    idAttribute: 'id'
+});
+
+var CommentCollection = Backbone.Collection.extend({
+    url: '/api/comments/',
+    model: CommentModel
+});
+
 var PostCollection = Backbone.Collection.extend({
     url: '/api/posts/',
     model: PostModel
 });
 
+var SubbredditCollection = Backbone.Collection.extend({
+    url: '/api/subbreddits/',
+    model: SubbredditModel
+});
+
 var PostItemView = Backbone.View.extend({
-    el:'<div></div>',
+    el:'<div class="hello"></div>',
 
     template: _.template('<h2><%= post.get("title") %></h2>'),
 
     render: function() {
         this.$el.html(this.template({ post: this.model }));
+    }
+});
+
+var PostsListView = Backbone.View.extend({
+    el: '<ul></ul>',
+
+    template: undefined,
+
+    render: function() {
+        var that = this;
+        this.collection.each(function(postModel) {
+            var postItemView = new PostItemView({ model: postModel});
+            postItemView.render();
+            this.el.append(postItemView.el);
+        });
     }
 });
 
